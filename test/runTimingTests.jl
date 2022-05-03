a004b064882489c27b3cc88014e451b805a16bdf
Gus Hart <gus.hart@gmail.com>
2022-05-03 17:01:16 -0600

    Added timing tests file for local machine, modified timing tests for CI runs

--- test/runTimingTests.jl
+++ test/runTimingTests.jl
@@ -2,8 +2,11 @@ using Spacey
 using Test
 using BenchmarkTools
 
+# These tests were originally part of the set that run automatically, but the difference between
+# "fast" and "slow" algorithms is much smaller on the github virtual machines. So keeping these
+# timing tests separate so I can compare my own machine times when updated algorithms
 @testset "Spacey.jl" begin
-    Δ = 0.2 # Fractional change in speedup between fast and slow algorithms that triggers an error
+    Δ=Δ # Fractional change in speedup that triggers an error
     #ideal hex lattice, 60° between basal plane vectors
     u = [1, 0, 0]
     v = [0.5, √3 / 2, 0]
@@ -20,25 +23,29 @@ using BenchmarkTools
     # hexagonal lattice in unique orientation
     g, h, i = threeDrotation([1, 0, 0], [0.5, √3 / 2, 0], [0, 0, √(8 / 3)], π / 7, π / 11, π / 3)
     @test length(pointGroup_basic(g, h, i)) == 24
-    # Add a timing check against basic and "fast" make sure fast is fast
+    # Add a timing check against basic and "fast"; make sure fast is fast
     # hexagonal case
     slow = @belapsed pointGroup_basic($g, $h, $i)
     fast = @belapsed pointGroup($g, $h, $i)
-    @test isapprox(slow / fast, 16, rtol=Δ)
+    println("actual speed up: ", round(slow/fast,digits=2))
+    @test isapprox(slow / fast, 27, rtol=Δ)
     # cubic case
     slow = @belapsed pointGroup_basic($d, $e, $f)
     fast = @belapsed pointGroup($d, $e, $f)
-    @test isapprox(slow / fast, 20, rtol=Δ)
+    println("actual speed up: ", round(slow / fast,digits=2))
+    @test isapprox(slow / fast, 53, rtol=Δ)
     # tetragonal case
     d, e, f = threeDrotation([1.1, 0, 0], [0, 1, 0], [0, 0, 1], π / 3, π / 5, π / 7)
     slow = @belapsed pointGroup_basic($d, $e, $f)
     fast = @belapsed pointGroup($d, $e, $f)
-    @test isapprox(slow / fast, 21, rtol=Δ)
+    println("actual speed up: ", round(slow / fast,digits=2))
+    @test isapprox(slow / fast, 61, rtol=Δ)
     # orthorhombic case
     d, e, f = threeDrotation([1.1, 0, 0], [0, 0.9, 0], [0, 0, 1], π / 3, π / 5, π / 7)
     slow = @belapsed pointGroup_basic($d, $e, $f)
     fast = @belapsed pointGroup($d, $e, $f)
-    @test isapprox(slow / fast, 20, rtol=Δ)
+    println("actual speed up: ", round(slow / fast,digits=2))
+    @test isapprox(slow / fast, 64, rtol=Δ)
     u = [1, 1, 2]
     v = [1, 2, 1]
     w = [2, 1, 1]
