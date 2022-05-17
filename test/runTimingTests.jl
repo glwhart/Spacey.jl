b6a6ecf218f7a9f0a42361e6cb994a139bac3fd8
Gus Hart <gus.hart@gmail.com>
2022-05-17 11:26:44 -0600

    Updated name for fast algo in runTimingTest.jl

--- test/runTimingTests.jl
+++ test/runTimingTests.jl
@@ -26,31 +26,31 @@ using BenchmarkTools
     # Add a timing check against basic and "fast"; make sure fast is fast
     # hexagonal case
     slow = @belapsed pointGroup_simple($g, $h, $i)
-    fast = @belapsed pointGroup($g, $h, $i)
+    fast = @belapsed pointGroup_fast($g, $h, $i)
     println("actual speed up: ", round(slow/fast,digits=2))
     @test isapprox(slow / fast, 27, rtol=Δ)
     # cubic case
     slow = @belapsed pointGroup_simple($d, $e, $f)
-    fast = @belapsed pointGroup($d, $e, $f)
+    fast = @belapsed pointGroup_fast($d, $e, $f)
     println("actual speed up: ", round(slow / fast,digits=2))
     @test isapprox(slow / fast, 53, rtol=Δ)
     # tetragonal case
     d, e, f = threeDrotation([1.1, 0, 0], [0, 1, 0], [0, 0, 1], π / 3, π / 5, π / 7)
     slow = @belapsed pointGroup_simple($d, $e, $f)
-    fast = @belapsed pointGroup($d, $e, $f)
+    fast = @belapsed pointGroup_fast($d, $e, $f)
     println("actual speed up: ", round(slow / fast,digits=2))
     @test isapprox(slow / fast, 61, rtol=Δ)
     # orthorhombic case
     d, e, f = threeDrotation([1.1, 0, 0], [0, 0.9, 0], [0, 0, 1], π / 3, π / 5, π / 7)
     slow = @belapsed pointGroup_simple($d, $e, $f)
-    fast = @belapsed pointGroup($d, $e, $f)
+    fast = @belapsed pointGroup_fast($d, $e, $f)
     println("actual speed up: ", round(slow / fast,digits=2))
     @test isapprox(slow / fast, 64, rtol=Δ)
     u = [1, 1, 2]
     v = [1, 2, 1]
     w = [2, 1, 1]
     u, v, w = threeDrotation(u, v, w, π / 3, π / 5, π / 7)
-    @test length(pointGroup(u, v, w)) == 12
+    @test length(pointGroup_fast(u, v, w)) == 12
     # Faster version
     slow = @belapsed pointGroup_simple($g, $h, $i)
     fast = @belapsed pointGroup_fast($g, $h, $i)
